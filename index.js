51b34f33ace86799f16dfafb0882c7a932ee5de4
colehenry <crhenry81@gmail.com>
2022-04-30 12:58:46 -0400

    minimist and changed database location

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 
-const db = require("./database.js")
+const db = require("./src/services/database.js")
 const app = express()
 
 app.use(express.urlencoded({ extended: true }));
